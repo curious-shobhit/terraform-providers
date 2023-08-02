e89d88d21d2d57ea28eaf4a3fd10f92576ee98cf
Shobhit Maheshwari <57742116+curious-shobhit@users.noreply.github.com>
2023-08-02 21:24:28 +0800

    Update create_container.sh

--- create_container.sh
+++ create_container.sh
@@ -1,22 +1,26 @@
-#!/bin/bash
-
-# Step 1: Build Docker container with SSH
-cat <<EOF >Dockerfile
+# Dockerfile
 FROM debian:latest
 
 RUN apt-get update \
     && apt-get install -y openssh-server \
     && mkdir /var/run/sshd \
-    && echo 'root:your_password' | chpasswd \
     && sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
-    && echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config
+    && sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config \
+    && echo "PermitUserEnvironment yes" >> /etc/ssh/sshd_config \
+    && echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
 EXPOSE 22
 
 CMD ["/usr/sbin/sshd", "-D"]
-EOF
+
+
+
+
+#############################################################################################
+
 
 docker build -t my_ssh_container .
+docker run -d -p 2222:22 -v /home/s996068:/home/s996068 --name my_ssh_container my_ssh_container
 
-# Step 2: Run Docker container on the EC2 instance
-docker run -d -p 2222:22 --name my_ssh_container my_ssh_container
+ssh -p 2222 s996068@10.205.10.14
+
